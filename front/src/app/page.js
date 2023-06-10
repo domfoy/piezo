bbdfa6f714e7929373ec334d7a36addf0d41c2f8
quentin.pergeline <quentin.pergeline@iwecloud.com>
2023-06-10 16:11:23 +0200

    feat(ui): inits station graph

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,93 +1,37 @@
 "use client"
 
+import h337 from 'heatmap.js';
 import {useEffect, useRef} from 'react';
-import styles from './page.module.css';
-import Chart from 'chart.js/auto';
 
-// import data from './data/09724X0023_P2_processed.json';
-import data from './data/03423X0056_100_processed.json';
+const points = [...Array(10).keys()]
+  .map((v, index) => ({
+    x: 100 + 500 * index / 10,
+    y: 200,
+    value: index % 2
+      ? 100
+      : 50
+  }));
 
-function clamp(min, max) {
-  return value => {
-    if (value < min) {
-      return min;
-    }
-    if (value > max) {
-      return max;
-    }
-    return value;
-  };
-}
-
-const claimPercent = clamp(0, 100);
-
-console.log('data', data)
-
-const PERCENTILES = {
-  '50': {
-    name: 'percentile50',
-    color: 'rgb(0, 100, 200)'
-  },
-  '75': {
-    name: 'percentile75',
-    color: 'rgb(0, 50, 220)'
-  },
-  // '90': 'percentile90'
+const testData = {
+  max: 100,
+  min: 0,
+  data: points
 };
 
+
 export default function Home() {
-  const canvasRef = useRef(null);
-
-  const renderChart = () => {
-    if (!canvasRef.current) {
-      return;
-    }
-    const percentileDataSets = [];
-
-    for (const percentileKey in PERCENTILES) {
-      percentileDataSets.push({
-        label: `high-${percentileKey}th percentile to median`,
-        data: data.stats.map(row => claimPercent(100 * (row.median + row[PERCENTILES[percentileKey].name]))),
-        borderColor: PERCENTILES[percentileKey].color
-      });
-      percentileDataSets.push({
-        label: `low-${percentileKey}th percentile to median`,
-        data: data.stats.map(row => claimPercent(100 * (row.median - row[PERCENTILES[percentileKey].name]))),
-        borderColor: PERCENTILES[percentileKey].color
-      });
-    }
-    const chartConfig = {
-      type: 'line',
-      data: {
-        labels: data.stats.map((row, id) => id),
-        datasets: [
-          {
-            label: 'Current year',
-            data: data.current_year.map(row => row.percent * 100),
-            borderColor: 'rgb(255, 0, 0)'
-          },
-          {
-            label: 'Median',
-            data: data.stats.map(row => row.median * 100),
-            borderColor: 'rgb(0, 255, 255)'
-          },
-          ...percentileDataSets
-        ]
-      }
-    };
-
-    new Chart(canvasRef.current, chartConfig);
-  };
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    renderChart();
+    const heatMapConfig = {
+      container: containerRef.current
+    };
+    const instance = h337.create(heatMapConfig);
 
-    return () => destroyChart();
-  }, []);
+    instance.setData(testData);
+  });
 
-  return (
-    <main className={styles.main}>
-      <canvas ref={canvasRef}></canvas>
-    </main>
-  )
+  return <div ref={containerRef}>
+    <canvas width="800" height="600"></canvas>
+  </div>
 }
